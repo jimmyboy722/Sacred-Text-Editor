ffff94cbf8f64b685d7562981c395b54cc956864
jimmyboy722 <jeckman722@gmail.com>
2024-11-10 09:23:17 -0500

    finshed debugging

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -5,9 +5,9 @@ let deferredPrompt;
 
 window.addEventListener("beforeinstallprompt", (event) => {
   // PREVENTING THE PROMPT FROM APPEARING ON MOBILES
-  event.preventDefault();
+  //  event.preventDefault();
   // STORING THE EVENT FOR USE LATER
-  deferredPrompt = event;
+  window.deferredPrompt = event;
   // SHOWING THE INSTALL BUTTON
   butInstall.style.display = "block";
   console.log("beforeinstallprompt event triggered");
@@ -15,16 +15,17 @@ window.addEventListener("beforeinstallprompt", (event) => {
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener("click", async () => {
-  if (!deferredPrompt) {
-    return;
-  }
+  // if (!deferredPrompt) {
+  //    return;
+  //  }
   // SHOWING THE PROMPT
-  deferredPrompt.prompt();
+  console.log("window.deferred prompt", window.deferredPrompt);
+  window.deferredPrompt.prompt();
   // WAITING FOR THE USER TO REPLY TO THE PROMPT
-  const { outcome } = await deferredPrompt.userChoice;
-  console.log(`User response: ${outcome}`);
+  // const { outcome } = await deferredPrompt.userChoice;
+  //console.log(`User response: ${outcome}`);
   // CLEARING THE PROMPT VALUE, SINCE IT CAN ONLY BE USED ONCE
-  deferredPrompt = null;
+  window.deferredPrompt = null;
   // HIDE THE INSTALL BUTTON
   butInstall.style.display = "none";
 });
@@ -33,5 +34,6 @@ butInstall.addEventListener("click", async () => {
 window.addEventListener("appinstalled", (event) => {
   console.log("App successfully installed", event);
   // HIDING THE INSTALL BUTTON, SINCE THE APP HAS BEEN INSTALLED
+  window.deferredPrompt = null;
   butInstall.style.display = "none";
 });
--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -29,6 +29,25 @@ module.exports = () => {
         swSrc: "./src-sw.js",
         swDest: "src-sw.js",
       }),
+      // Creates a manifest.json file.
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "Just Another Text Editor",
+        short_name: "JATE",
+        description: "Add text to editor!",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
+        start_url: "/",
+        publicPath: "/",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
     ],
 
     module: {
